fe4a302d03c54ecca520cafdbaf5019c40387e1f
elusive7733 <abdullaharaheel@gmail.com>
2024-07-05 15:46:32 +0500

    fixed metadata

--- app/(chat)/chat/[id]/page.tsx
+++ app/(chat)/chat/[id]/page.tsx
@@ -1,4 +1,3 @@
-import { type Metadata } from 'next'
 import { notFound, redirect } from 'next/navigation'
 
 import { auth } from '@/auth'
@@ -13,20 +12,6 @@ export interface ChatPageProps {
   }
 }
 
-export async function generateMetadata({
-  params
-}: ChatPageProps): Promise<Metadata> {
-  const session = await auth()
-
-  if (!session?.user) {
-    return {}
-  }
-
-  const chat = await getChat(params.id, session.user.id)
-  return {
-    title: chat?.title.toString().slice(0, 50) ?? 'Chat'
-  }
-}
 
 export default async function ChatPage({ params }: ChatPageProps) {
   const session = (await auth()) as Session
