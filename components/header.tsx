9594cf1feca072c483a0f12318a0295d9f3e2aa7
elusive7733 <abdullaharaheel@gmail.com>
2024-07-05 13:29:34 +0500

    Changed Header

--- components/header.tsx
+++ components/header.tsx
@@ -1,14 +1,12 @@
 import * as React from 'react'
 import Link from 'next/link'
 
-import { cn } from '@/lib/utils'
 import { auth } from '@/auth'
-import { Button, buttonVariants } from '@/components/ui/button'
+import { Button } from '@/components/ui/button'
 import {
-  IconGitHub,
   IconNextChat,
+  IconOpenAI,
   IconSeparator,
-  IconVercel
 } from '@/components/ui/icons'
 import { UserMenu } from '@/components/user-menu'
 import { SidebarMobile } from './sidebar-mobile'
@@ -29,8 +27,8 @@ async function UserOrLogin() {
         </>
       ) : (
         <Link href="/new" rel="nofollow">
-          <IconNextChat className="size-6 mr-2 dark:hidden" inverted />
-          <IconNextChat className="hidden size-6 mr-2 dark:block" />
+          <IconOpenAI className="size-6 mr-2 dark:hidden" />
+          <IconOpenAI className="hidden size-6 mr-2 dark:block" />
         </Link>
       )}
       <div className="flex items-center">
