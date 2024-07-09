b5cb56f05331d3ecc7d7160cb22c13650c97d41c
Elusive7733 <abdullaharaheel@gmail.com>
2024-07-09 21:58:30 +0500

    Files UI complete

--- components/files/list-files.tsx
+++ components/files/list-files.tsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import { Button } from '../ui/button'
+import { FaRegTrashAlt } from 'react-icons/fa'
 
-const listFiles = () => {
+interface ListFilesProps {
+  files: string[]
+}
+
+const ListFiles = ({ files }: ListFilesProps) => {
+  if (!files || files.length === 0) return <div>No files</div>
+
+  const handleDeleteClick = (fileName: string) => {
+    console.log(fileName)
+  }
+
   return (
-    <div>listFiles</div>
+    <>
+      {files.map((file, index) => (
+        <li
+          key={index}
+          className="flex justify-between items-center p-1 text-sm font-normal"
+        >
+          <span>{file}</span>
+
+          <FaRegTrashAlt
+            className="size-4 text-red-600 hover:text-red-900 cursor-pointer"
+            onClick={() => handleDeleteClick(file)}
+          />
+        </li>
+      ))}
+    </>
   )
 }
 
-export default listFiles
+export default ListFiles
--- components/files/project-files.tsx
+++ components/files/project-files.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import { files } from '@/data/dummyData'
+import ListFiles from './list-files'
 
 interface ProjectFilesProps {
   currentProject: string
@@ -10,40 +11,36 @@ const ProjectFiles = ({ currentProject }: ProjectFilesProps) => {
   let projectFiles = files[currentProject]
 
   return (
-    <div>
-      <div>
-        Meeting Transcripts
-        <ul>
-          {projectFiles.meetingTranscripts.map((file, index) => (
-            <li key={index}>{file}</li>
-          ))}
+    <div className="w-full px-4 mx-3">
+      <div className="m-2 border border-gray-600/40 p-2 rounded-lg">
+        <h3 className="font-medium">Meeting Transcripts</h3>
+        <hr />
+        <ul className="mt-2">
+          <ListFiles files={projectFiles.meetingTranscripts}  />
         </ul>
       </div>
 
-      <div>
-        Emails
-        <ul>
-          {projectFiles.emails.map((file, index) => (
-            <li key={index}>{file}</li>
-          ))}
+      <div className="m-1 border border-gray-600/40 p-4 rounded-lg">
+        <h3 className="font-medium">Emails</h3>
+        <hr />
+        <ul className="mt-2">
+          <ListFiles files={projectFiles.emails} />
         </ul>
       </div>
 
-      <div>
-        Jotforms
-        <ul>
-          {projectFiles.jotforms.map((file, index) => (
-            <li key={index}>{file}</li>
-          ))}
+      <div className="m-1 border border-gray-600/40 p-4 rounded-lg">
+        <h3 className="font-medium">Jotforms</h3>
+        <hr />
+        <ul className="mt-2">
+          <ListFiles files={projectFiles.jotforms} />
         </ul>
       </div>
 
-      <div>
-        Other
-        <ul>
-          {projectFiles.other.map((file, index) => (
-            <li key={index}>{file}</li>
-          ))}
+      <div className="m-1 border border-gray-600/40 p-4 rounded-lg">
+        <h3 className="font-medium">Others</h3>
+        <hr />
+        <ul className="mt-2">
+          <ListFiles files={projectFiles.other} />
         </ul>
       </div>
     </div>
