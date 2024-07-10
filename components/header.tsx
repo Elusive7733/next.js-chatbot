2e96d538315fcef33cefd12f31e94066363e2507
elusive7733 <abdullaharaheel@gmail.com>
2024-07-10 16:17:59 +0500

    refactor: Remove commented out code for login in header component

--- components/header.tsx
+++ components/header.tsx
@@ -40,17 +40,18 @@ async function UserOrLogin() {
           />
         </Link>
       )}
-      <div className="flex items-center">
+
+      {/* <div className="flex items-center">
         <IconSeparator className="size-6 text-muted-foreground/50" />
         {session?.user ? (
           <UserMenu user={session.user} />
         ) : (
-          // <Button variant="link" asChild className="-ml-2">
-          //   <Link href="/login">Login</Link>
-          // </Button>
-          null
+          <Button variant="link" asChild className="-ml-2">
+             <Link href="/login">Login</Link>
+           </Button>
+          
         )}
-      </div>
+      </div> */}
     </>
   )
 }
