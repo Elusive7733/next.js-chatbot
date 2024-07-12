f4d829131773afb90c367fa0d05f826eddf33def
elusive7733 <abdullaharaheel@gmail.com>
2024-07-12 16:06:58 +0500

    clear messages on switching projects

--- components/chat-panel.tsx
+++ components/chat-panel.tsx
@@ -8,6 +8,7 @@ import { IconShare } from '@/components/ui/icons'
 import { ChatShareDialog } from '@/components/chat-share-dialog'
 import { useAIState, useActions, useUIState } from 'ai/rsc'
 import type { AI } from '@/lib/chat/actions'
+import { useProjectStore } from '@/lib/store/store'
 
 export interface ChatPanelProps {
   id?: string
@@ -30,6 +31,14 @@ export function ChatPanel({
   const [messages, setMessages] = useUIState<typeof AI>()
   const { submitUserMessage } = useActions()
   const [shareDialogOpen, setShareDialogOpen] = React.useState(false)
+  const { currentProjectId } = useProjectStore()
+
+  React.useEffect(() => {
+    if (currentProjectId) {
+      setMessages([])
+    }
+    console.log('USEEFFECT RAN')
+  }, [currentProjectId, setMessages])
 
   return (
     <div className="fixed inset-x-0 bottom-0 w-full bg-gradient-to-b from-muted/30 from-0% to-muted/30 to-50% duration-300 ease-in-out animate-in dark:from-background/10 dark:from-10% dark:to-background/80 peer-[[data-state=open]]:group-[]:lg:pl-[250px] peer-[[data-state=open]]:group-[]:xl:pl-[300px]">
@@ -70,7 +79,6 @@ export function ChatPanel({
 
         <div className="space-y-4 border-t bg-background px-4 py-2 shadow-lg sm:rounded-t-xl sm:border md:py-4">
           <PromptForm input={input} setInput={setInput} />
-    
         </div>
       </div>
     </div>
